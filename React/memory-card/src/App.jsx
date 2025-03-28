990ceff0b1ba5535d1a03157de5f301cacb3d440
oswaldo <oswaldo32gonzalez@gmail.com>
2025-03-27 22:13:09 -0600

    Add memory card project

---
+++ React/memory-card/src/App.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react"
+import './app.css'
+
+export function App() {
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array
+  } 
+
+  function resetGame() {
+    setScore(0)
+    setLastPick([])
+    setDiscoveredCards([])
+  }
+
+  function showCard() {
+    
+  }
+
+  function playRound(newCard, index) {
+    
+    if(discoveredCards.includes(newCard)) return
+    if (!lastPick.length) {
+      setLastPick([newCard, index])
+    } else if(lastPick[1] === index) {
+      return
+    }else if(newCard === lastPick[0]) {
+      setScore(prev => prev + 1)
+      setDiscoveredCards(prev => [...prev, newCard])
+      setLastPick([])
+    } else {
+      resetGame()
+    }
+  }
+
+  const [deck, setDeck] = useState([])
+  const [score, setScore] = useState(0)
+  const [lastPick, setLastPick] = useState([])
+  const [discoveredCards, setDiscoveredCards] = useState([])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+      .then(res => res.json())
+      .then(async (data) => {
+        const cards = data.results;
+        // Fetch all images in parallel
+        const pokemonData = await Promise.all(
+          cards.map(async (card) => {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${card.name}/`);
+            const ndData = await res.json();
+            return { name: card.name, url: ndData.sprites.front_default };
+          })
+        );
+        setDeck(shuffleArray([...pokemonData, ...pokemonData]));
+      });
+  }, []);
+  
+
+  return (
+    <main>
+      <h1>Memory Card</h1>
+      <h2>{`Score: ${score}`}</h2>
+      <section className="cardsContainer">
+        {
+          deck && 
+          deck.map((card, index) => (
+            <div 
+            style={
+              discoveredCards.includes(card.name) ?
+              {
+                backgroundImage: `url(${card.url})`
+              } :
+              null
+            }
+            className="card"
+            key={index}
+            onClick={() => {
+              playRound(card.name, index)
+            }}
+            >
+            </div>
+          ))
+        }
+      </section>
+    </main>
+  )
+}
