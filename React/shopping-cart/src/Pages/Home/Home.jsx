923976101fac5e33bd3f1c49750d7f93c7aef3b3
oswaldo <oswaldo32gonzalez@gmail.com>
2025-04-11 13:00:29 -0600

    Add Basic Home page with simple web description

--- React/shopping-cart/src/Pages/Home/Home.jsx
+++ React/shopping-cart/src/Pages/Home/Home.jsx
@@ -1,3 +1,21 @@
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <p>This is the Home Page</p>;
+  return (
+    <main className={styles.Home}>
+      <div className={styles.container}>
+        <h2>The Shopping Cart Project</h2>
+        <p>
+          This website is created to practice the knowledge aquired with The
+          Odin Project React section. The intention of this is to be able to
+          implement the following:
+        </p>
+        <ul>
+          <li className={styles.listItem}>React Router</li>
+          <li className={styles.listItem}>React Testing with Vitest</li>
+          <li className={styles.listItem}>React Fetching with useEffect</li>
+        </ul>
+      </div>
+    </main>
+  );
 }
