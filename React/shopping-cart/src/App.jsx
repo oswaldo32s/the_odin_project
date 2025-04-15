e634f906e0d998ba44fee1ddd8c900266d4eacc6
oswaldo <oswaldo32gonzalez@gmail.com>
2025-04-14 21:48:41 -0600

    Add Shopping container and items

--- React/shopping-cart/src/App.jsx
+++ React/shopping-cart/src/App.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Header from "./Pages/Core/Header/Header";
 import "./App.css";
 
 function App() {
+  const [shoppingItems, setShoppingItems] = useState([]);
+
+  useEffect(() => {
+    const fakeStoreURL = "https://fakestoreapi.com/products";
+    fetch(fakeStoreURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setShoppingItems(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet context={[shoppingItems]} />
       <footer></footer>
     </>
   );
--- React/shopping-cart/src/Pages/Shopping/Shopping.jsx
+++ React/shopping-cart/src/Pages/Shopping/Shopping.jsx
@@ -1,12 +1,42 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useOutletContext } from "react-router-dom";
+import styles from "./shopping.module.css";
 
 export default function Shopping() {
+  const [shoppingItems] = useOutletContext();
+  const [shoppingCart, setShoppingCart] = useState([]);
+
   return (
-    <>
-      <p>This is the Shopping Page</p>
-      <button>
-        <Link to="/">Go Back Home</Link>
-      </button>
-    </>
+    <main className={styles.Shopping}>
+      <div className={styles.container}>
+        <p>This is the Shopping Page</p>
+        <div className={styles.shoppingContainer}>
+          {shoppingItems.map((item) => (
+            <section key={item.id} className={styles.shoppingItem}>
+              <div className={styles.shoppingImgContainer}>
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className={styles.shoppingItemImg}
+                />
+              </div>
+              <article className={styles.article}>
+                <div className={styles.truncateTitle}>
+                  <h3 className={styles.shoppingItemTitle}>{item.title}</h3>
+                </div>
+                <div className={styles.truncateDesc}>
+                  <p className={styles.shoppingItemDesc}>{item.description}</p>
+                </div>
+                <p className={styles.shoppingItemPrice}>$ {item.price}</p>
+              </article>
+              <button type="button">Add to Cart</button>
+            </section>
+          ))}
+        </div>
+        <button>
+          <Link to="/">Go Back Home</Link>
+        </button>
+      </div>
+    </main>
   );
 }
